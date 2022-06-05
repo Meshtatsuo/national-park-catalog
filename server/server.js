7967dfd917e947a1b80c662c2ed6bc38228b19a5
meshtatsuo <meshtatsuo@gmail.com>
2022-06-05 12:40:55 -0600

    another fix

--- server/server.js
+++ server/server.js
@@ -14,11 +14,14 @@ app.use(
   })
 );
 
-app.use(routes);
-
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+app.use(routes);
 
 app.listen(PORT, () => console.log("Now listening on port ", PORT));
